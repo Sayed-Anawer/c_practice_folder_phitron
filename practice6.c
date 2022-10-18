682ff6d6bd2c39f3c73ff12c40ca529187f86f2b
Sayed-Anawer <sayedanawer99@gmail.com>
2022-10-18 22:57:43 +0600

    Practice6 file is added

---
+++ practice6.c
@@ -0,0 +1,12 @@
+// take a integer and the input will contain % sign and the output will be the integer
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d", a);
+    return 0;
+}
+
