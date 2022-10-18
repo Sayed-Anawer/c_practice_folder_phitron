171cd8a9d7ea66c002de0ad15672ff668739e645
Sayed-Anawer <sayedanawer99@gmail.com>
2022-10-18 21:10:26 +0600

    practice1 file added

---
+++ practice1.c
@@ -0,0 +1,8 @@
+// problem 1 answer:
+#include <stdio.h>
+
+int main() {
+    printf("Hi there /\\.\tI hope you are doing great and practice well.\nNow this is a simple task you need to do.\tJust care for the identation.\nShow the 100%% of sincerity.");
+
+    return 0;
+}
