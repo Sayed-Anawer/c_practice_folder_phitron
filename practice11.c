f0a79653f9ae248df809ca0b1ae18173c17d89bc
Sayed-Anawer <sayedanawer99@gmail.com>
2022-10-19 15:30:39 +0600

    Users can vote or not

---
+++ practice11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if(age>=18){
+        printf("Congratulation! You can vote\n");
+    }
+    else{
+        printf("You cannot vote\n");
+    }
+    return 0;
+ }
