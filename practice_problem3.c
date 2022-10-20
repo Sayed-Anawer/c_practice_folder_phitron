c4f3f8997e8365cc319ada5034e95ffb1d9e1f76
Sayed-Anawer <sayedanawer99@gmail.com>
2022-10-20 22:00:57 +0600

    Practice_Problem3 is added

---
+++ practice_problem3.c
@@ -0,0 +1,36 @@
+/**************************************************************** 
+ * Mr. A  has a grid that has N rows and M columns. Each row is numbered from 1 to N from top to bottom. Each column is numbered from 1 to M from left to right.
+Each tile in the grid contains a number. The numbers are arranged as follows:
+Row 1 contains integers from 1 to M from left to right.
+Row 2 contains integers from M+1 to 2×M from left to right.
+Row 3 contains integers from 2×M+1 to 3×M from left to right.
+And so on until row N.
+A domino is defined as two different tiles in the grid that touch by their sides. A domino is said to be tight if and only if the two numbers in the domino have a difference of exactly 1. Count the number of distinct tight dominoes in the grid.
+ 
+Two dominoes are said to be distinct if and only if there exists at least one tile that is in one domino, but not in the other.
+ 
+The range of the  M,N-> 1-100 (Positive Value)
+
+******************************************************************/
+
+
+#include<stdio.h>
+
+int main(){
+
+    int N,M,count=0;
+    printf("Enter the value of N: ");
+    scanf("%d",&N);
+    printf("Enter the value of M: ");
+    scanf("%d",&M);
+    if(M==1 && N!=1){
+        printf("%d",N-1);
+    }
+    else if(N==1 && M>1){
+        printf("%d",M-1);
+    }
+    else if(N>1 && M>1){
+        printf("%d", (N*M)-N);
+    }
+
+}
